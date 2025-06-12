5a3ba954536fdecc26c440b865db20b8da45e6f4
sclas6 <nagai.yuta.19@shizuoka.ac.jp>
2025-06-12 10:40:40 +0900

    Update 0.5.1 [BugFix]

--- src/main.cpp
+++ src/main.cpp
@@ -21,58 +21,54 @@ String sensorReadings;
 
 bool httpGetAndUpdateEpaper()
 {
-  char url[256] = {0};
-  char userAgent[256] = {0};
-  sprintf(url, "%s", HTTP_ENDPOINT);
-  sprintf(userAgent, "%s %s %s", __FILE__, __DATE__, __TIME__);
-  //
-  Serial.printf("HTTP GET: %s\n", url);
-  http.setUserAgent(userAgent);
-  http.begin(url);
-  //
-  int code = http.GET();
-  if (code != 200) {
-    Serial.printf("HTTP Status: %d\r\n", code);
-    return false;
-  }
-  //
-  int len = http.getSize();
-  if (len == 0) {
-    Serial.printf("Content-Length: %d\r\n", len);
-    return false;
-  }
-  // 1. データ送信開始
-  epd.SendCommand(0x10);
-  //
-  WiFiClient *stream = http.getStreamPtr();
-  int readSize = 0;
-  while (http.connected() && (len > 0 || len == -1))
-  {
-    size_t size = stream->available();
-    if (size) {
-      int c = stream->readBytes(httpBuff, min(sizeof(httpBuff), size));
+    char url[256] = {0};
+    char userAgent[256] = {0};
+    sprintf(url, "%s", HTTP_ENDPOINT);
+    sprintf(userAgent, "%s %s %s", __FILE__, __DATE__, __TIME__);
 
-      for (int i = 0; i < c; i++) {
-        // 2. 画素データ送信
-        epd.SendData(httpBuff[i]);
-        M5.Display.print(httpBuff[i]);
-      }
-      if (len > 0) {
-        len -= c;
-        readSize += c;
-      }
-      Serial.print(".");
+    Serial.printf("HTTP GET: %s\n", url);
+    http.setUserAgent(userAgent);
+    http.begin(url);
+
+    int code = http.GET();
+    if (code != 200) {
+        Serial.printf("HTTP Status: %d\r\n", code);
+        return false;
     }
-    delay(1);
-  }
-  Serial.println("");
-  // 3. ディスプレイON
-  epd.TurnOnDisplay();
-  http.end();
-  // 4. スリープ
-  epd.Sleep();
-  Serial.printf("httpGet(): Done. readSize=%d\n", readSize);
-  return true;
+
+    int len = http.getSize();
+    if (len == 0) {
+        Serial.printf("Content-Length: %d\r\n", len);
+        return false;
+    }
+
+    epd.SendCommand(0x10);
+    WiFiClient *stream = http.getStreamPtr();
+    int readSize = 0;
+    while (http.connected() && (len > 0 || len == -1))
+    {
+        size_t size = stream->available();
+        if (size) {
+        int c = stream->readBytes(httpBuff, min(sizeof(httpBuff), size));
+
+        for (int i = 0; i < c; i++) {
+            epd.SendData(httpBuff[i]);
+            M5.Display.print(httpBuff[i]);
+        }
+        if (len > 0) {
+            len -= c;
+            readSize += c;
+        }
+        Serial.print(".");
+        }
+        delay(1);
+    }
+    Serial.println("");
+    epd.TurnOnDisplay();
+    http.end();
+    epd.Sleep();
+    Serial.printf("httpGet(): Done. readSize=%d\n", readSize);
+    return true;
 }
 
 void setup() {
